6f27c9f35ecad9e33ef82abf6ef485127b7a2fd5
Gowtham Kumar <gowthamkumar20200@gmail.com>
2021-12-11 16:04:58 +0530

    Added: Inital setup for websocket

--- chartroom/settings.py
+++ chartroom/settings.py
@@ -112,3 +112,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = "chartroom.routing.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,41 @@
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'tester'
+            }
+        )
+
+    # async def receive(self, text_data=None):
+
+    #     text_data_json = json.loads(text_data)
+    #     tester = text_data_json['tester']
+    
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(text_data=json.dumps({
+            'tester': tester
+        }))
+    
+    async def disconnect(self, code):
+        return await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
